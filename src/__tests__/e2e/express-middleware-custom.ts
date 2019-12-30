4a34a6664ab90965c4c1a8965e6bbebfd57bdc3c
miguemasx <migue.masx@gmail.com>
2019-12-30 16:43:56 +0100

    rename file

--- src/__tests__/e2e/express-middleware-custom.ts
+++ src/__tests__/e2e/express-middleware-custom.ts
@@ -1,50 +1,31 @@
 import { ContextService } from '../../ContextService';
 import Logger from '../../index';
-
 import * as request from 'supertest';
 import * as express from 'express';
-import * as uuid from 'uuid/v4';
-jest.mock('uuid/v4');
 
-describe('App middleware', () => {
+describe('App middleware custom', () => {
   let app;
   beforeEach(() => {
-    app = undefined;
     app = express();
     app.use(ContextService.middlewareRequest());
-    app.use(ContextService.middlewareRequestUUID());
-
-    const service = async () => {
-      Logger.log('async log');
-    };
+    app.use(
+      ContextService.middleware((context, req, res) => {
+        context.set('request:ip', req.ip);
+      }),
+    );
 
     app.get('/test', async (req, res) => {
       Logger.log('Start request');
-      await service();
       res.send('');
     });
   });
-
-  it('generate uuid per request', async () => {
+  it('trace with ip', async () => {
     const spy = jest.spyOn(console, 'log').mockImplementation(jest.fn());
 
     await request(app)
       .get('/test')
       .expect(200);
-    expect(spy).toBeCalled();
 
-    // In the same request generate only one uuid
-    expect(uuid).toBeCalledTimes(1);
-    expect(spy).toBeCalledTimes(2);
-
-    await request(app)
-      .get('/test')
-      .expect(200);
-
-    expect(spy).toBeCalled();
-
-    // generate new uuid for new request
-    expect(uuid).toBeCalledTimes(2);
-    expect(spy).toBeCalledTimes(4);
+    expect(spy.mock.calls[0][0]).toEqual('[::ffff:127.0.0.1] Start request');
   });
 });
--- src/__tests__/e2e/express-middleware.ts
+++ src/__tests__/e2e/express-middleware.ts
@@ -1,31 +1,50 @@
 import { ContextService } from '../../ContextService';
 import Logger from '../../index';
+
 import * as request from 'supertest';
 import * as express from 'express';
+import * as uuid from 'uuid/v4';
+jest.mock('uuid/v4');
 
-describe('App middleware custom', () => {
+describe('App middleware', () => {
   let app;
   beforeEach(() => {
+    app = undefined;
     app = express();
     app.use(ContextService.middlewareRequest());
-    app.use(
-      ContextService.middleware((context, req, res) => {
-        context.set('request:ip', req.ip);
-      }),
-    );
+    app.use(ContextService.middlewareRequestUUID());
+
+    const service = async () => {
+      Logger.log('async log');
+    };
 
     app.get('/test', async (req, res) => {
       Logger.log('Start request');
+      await service();
       res.send('');
     });
   });
-  it('trace with ip', async () => {
+
+  it('generate uuid per request', async () => {
     const spy = jest.spyOn(console, 'log').mockImplementation(jest.fn());
 
     await request(app)
       .get('/test')
       .expect(200);
+    expect(spy).toBeCalled();
 
-    expect(spy.mock.calls[0][0]).toEqual('[::ffff:127.0.0.1] Start request');
+    // In the same request generate only one uuid
+    expect(uuid).toBeCalledTimes(1);
+    expect(spy).toBeCalledTimes(2);
+
+    await request(app)
+      .get('/test')
+      .expect(200);
+
+    expect(spy).toBeCalled();
+
+    // generate new uuid for new request
+    expect(uuid).toBeCalledTimes(2);
+    expect(spy).toBeCalledTimes(4);
   });
 });
